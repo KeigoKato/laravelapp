ec56b5e490880289c157c79a9e51acc8c552473b
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-07-07 21:49:34 +0900

    [add] アクションを追加する
    コントローラに定義したアクションを実行する

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,5 +6,31 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //
+  public function index() {
+    return <<<EOF
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>Hello/Index</title>
+    <style>
+      body {
+        font-size: 16pt;
+        color: #999;
+      }
+      h1 {
+        font-size: 100pt;
+        text-align: right;
+        color: #999;
+        margin: -40px 0 -50px 0;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Index</h1>
+    <p>これはHelloコントローラのindexアクションです。</p>
+  </body>
+</html>
+EOF;
+  }
 }
+?>
--- routes/web.php
+++ routes/web.php
@@ -21,35 +21,6 @@ Route::get('/', function () {
 //   return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
 // });
 
-
-// getリクエスト先のアドレスに入力したmsgの値がヒアドキュメント内の$msgに反映される
-Route::get("hello/{msg?}", function($msg="no message"){
-  $html = <<<EOF
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="utf-8">
-    <title>Hello</title>
-    <style>
-      body {
-        font-size: 16pt;
-        color: #999;
-      }
-      h1 {
-        font-size: 100pt;
-        text-align: right;
-        color: #eee;
-        margin: -40px 0 -50px 0;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>Hello</h1>
-    <p>{$msg}</p>
-    <p>これはサンプルで作ったページです。</p>
-  </body>
-</html>
-EOF;
-
-  return $html;
-});
+// helloにgetリクエストするとHelloContorollerのindexアクションを実行する。
+// ルーティングでviewを作成するような処理は書かないのがMVCの基本のひとつ。
+Route::get("hello", "HelloController@index");
