b9a43635ca5662c219b4840894c34d75ee287217
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-07-07 20:21:21 +0900

    [add] ヒアドキュメントを使う
    web.phpに書いたヒアドキュメントを使ってビューを表示する。

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,38 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+$html = <<<EOF
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>Hello</title>
+    <style>
+      body {
+        font-size: 16pt;
+        color: #999;
+      }
+      h1 {
+        font-size: 100pt;
+        text-align: right;
+        color: #eee;
+        margin: -40px 0 -50px 0;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Hello</h1>
+    <p>This is sample page.</p>
+    <p>これはサンプルで作ったページです。</p>
+  </body>
+</html>
+EOF;
+
 // /helloにアクセスするとreturnで返されるHTMLを出力する。
-Route::get("hello", function() {
-  return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+// Route::get("hello", function() {
+//   return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+// });
+
+Route::get("hello", function() use ($html) {
+  return $html;
 });
