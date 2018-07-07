0300980356f3020cd5be8720dc2962c87ba346a3
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-07-07 22:16:01 +0900

    [add] シングルアクションコントローラ
    アクションがひとつのコントローラの定義

--- routes/web.php
+++ routes/web.php
@@ -26,5 +26,5 @@ Route::get('/', function () {
 // /hello/123/456とアドレスを指定すれば123と456が変数に入る
 // Route::get("hello/{id?}/{pass?}", "HelloController@index");
 
-Route::get("hello", "HelloController@index");
-Route::get("hello/other", "HelloController@other");
+// シングルアクションコントローラにはアクション名は必要ない
+Route::get("hello", "HelloController");
