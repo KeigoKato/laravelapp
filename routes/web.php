5c47b61bf23d79636f227409659cfd1ac22eb520
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-07-07 20:13:31 +0900

    [add] ルート情報を追加する
    /helloにgetリクエストするとreturnで返されるHTMLを実行する。

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+// ルートアドレスにgetリクエストでアクセスしたらviewのwelcomeファイルを選択する。
 Route::get('/', function () {
     return view('welcome');
+});
+
+// /helloにアクセスするとreturnで返されるHTMLを出力する。
+Route::get("hello", function() {
+  return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
 });
