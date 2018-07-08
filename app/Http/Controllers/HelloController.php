8591383cc7dbdebeb122919e6f76f33654e2d480
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-07-08 16:28:46 +0900

    [add] @eachによるコレクションビュー
    文法がややこしいがこれは頻繁に使うはず

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,10 +10,10 @@ class HelloController extends Controller
   // クエリ文字列を受け取る場合はアクションメソッドの引数にRequestクラスのrequestインスタンスを渡す。
   public function index(Request $request) {
     $data = [
-      // "キー名"クエリの中身はrequestメソッドの"キー名"プロパティに入っている
-      "msg" => "これはコントローラから渡されたメッセージです。",
-      "id" => $request->id,
+      ["name"=>"山田たろう", "mail"=>"taro@yamada"],
+      ["name"=>"田中はなこ", "mail"=>"hanako@flower"],
+      ["name"=>"鈴木さちこ", "mail"=>"sachiko@happy"],
     ];
-    return view("hello.index", $data);
+    return view("hello.index", ["data"=>$data]);
   }
 }
