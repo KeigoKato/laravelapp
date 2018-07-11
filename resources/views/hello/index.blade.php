3a07cf0714a70bf56f23e9df5823296efb980959
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-07-11 20:44:28 +0900

    #001 index.blade.phpのコンフリクトを修正

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @extends("layouts.helloapp")
 
 @section("title", "Index")
@@ -28,40 +27,3 @@
 @section("footer")
 copyright 2017 tuyano.
 @endsection
-=======
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="utf-8">
-    <title>Hello/Index</title>
-    <style>
-      body {
-        font-size: 16pt;
-        color: #999;
-      }
-      h1 {
-        font-size: 50pt;
-        text-align: right;
-        color: #f6f6f6;
-        margin: -20px 0 -30px 0;
-        letter-spacing: -4pt;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>Blade/Index</h1>
-    <p>&#064;whileディレクティブの例</p>
-    <ol>
-      @php
-      $counter = 0;
-      @endphp
-      @while($counter < count($data))
-      <li>{{$data[$counter]}}</li>
-      @php
-      $counter++;
-      @endphp
-      @endwhile
-    </ol>
-  </body>
-</html>
->>>>>>> 6c71a5ff2cd979fdcae09971f60dbc1bdc291aa9
