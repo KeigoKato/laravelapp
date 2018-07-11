c687fcfec71416949e4f245693cb0dd1d5c1ad4f
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-07-11 20:28:00 +0900

    merge修正

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,6 @@ Route::get('/', function () {
 });
 
 Route::get("hello", "HelloController@index");
-<<<<<<< HEAD
-Route::post("hello", "HelloController@post");
-=======
 Route::get("hello/show", "HelloController@show");
 Route::post("hello", "HelloController@post");
 Route::get("hello/add", "HelloController@add");
@@ -18,4 +15,3 @@ Route::get("hello/edit", "HelloController@edit");
 Route::post("hello/edit", "HelloController@update");
 Route::get("hello/del", "HelloController@del");
 Route::post("hello/del", "HelloController@remove");
->>>>>>> C3_4_レイアウトの作成
