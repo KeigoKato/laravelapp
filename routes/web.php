52c698d786874126665acf8cf1fe212f6d8ca728
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-07-11 21:33:57 +0900

    #002 Personクラスメソッドによるid検索

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Route::get("hello/del", "HelloController@del");
 Route::post("hello/del", "HelloController@remove");
 
 Route::get("person", "PersonController@index");
+Route::get("person/find", "PersonController@find");
+Route::post("person/find", "PersonController@search");
