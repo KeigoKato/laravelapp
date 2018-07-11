655c431692b54beadb544e1fec84c3ece18c2748
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-07-11 20:40:39 +0900

    #001 シーダーファイルの修正

--- database/seeds/PeopleTableSeeder.php
+++ database/seeds/PeopleTableSeeder.php
@@ -16,21 +16,21 @@ class PeopleTableSeeder extends Seeder
         "mail" => "taro@yamada.jp",
         "age" => 12,
       ];
-      DB:table("people")->insert($param);
+      DB::table("people")->insert($param);
 
       $param = [
         "name" => "hanako",
         "mail" => "hanako@flower.jp",
         "age" => 34,
       ];
-      DB:table("people")->insert($param);
+      DB::table("people")->insert($param);
 
       $param = [
         "name" => "sachiko",
         "mail" => "sachiko@happy.jp",
         "age" => 56,
       ];
-      DB:table("people")->insert($param);
+      DB::table("people")->insert($param);
 
     }
 }
