e7884cf25a86b706b4dc488c2b1ddc0e8b75b756
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-07-12 20:53:36 +0900

    #003 ScopePersonクラスを作る

---
+++ app/Scopes/ScopePerson.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+use App\Scope\ScopePerson;
+
+class ScopePerson implements Scope {
+    public function apply(Builder $builder, Model $model) {
+        $builder->where("age", ">", 20);
+    }
+
+    protected static function boot() {
+        parent::boot();
+        static::addGlobalScope(new ScopePerson);
+    }
+}
+
+
+
+
