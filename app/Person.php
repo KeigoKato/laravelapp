61405908c9de8694e8c251999578dcfd552bb2ee
KeigoKato <keeeeeeeeigo1129@gmail.com>
2018-07-12 21:26:25 +0900

    f/004 モデルファイルを使ってaddとcreateアクションを実行

--- app/Person.php
+++ app/Person.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Builder;
 
 class Person extends Model
 {
+    protected $guarded = array("id");
+
     public function getData() {
         return $this->id.": ".$this->name."(".$this->age.")";
     }
@@ -14,19 +16,22 @@ class Person extends Model
     public function scopeNameEqual($query, $str) {
         return $query->where("name", $str);
     }
-
     public function scopeAgeGreaterThan($query, $n) {
         return $query->where("age", ">=", $n);
     }
-
     public function scopeAgeLessThan($query, $n) {
         return $query->where("age", "<=", $n);
     }
-
     protected static function boot() {
         parent::boot();
         static::addGlobalScope("age", function(Builder $builder){
             $builder->where("age", ">", 20);
         });
     }
+
+    public static $rules = array(
+        "name" => "required",
+        "mail" => "email",
+        "age" => "integer|min:0|max:150"
+    );
 }
